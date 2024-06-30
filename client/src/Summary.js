5e775dbacd0379013cd3c3628cc0927a7a6a3148
Guru <guru@duck.com>
2024-07-01 00:51:38 +0530

    typo

--- client/src/Summary.js
+++ client/src/Summary.js
@@ -16,7 +16,7 @@ const Summary = ({ codeTest, codeTrack, dt, dc, totalPoints }) => {
       <p>Code Test: {codeTest} x 30 = {codeTest * 30}</p>
       <p>Code Track: {codeTrack} x 2 = {codeTrack * 2}</p>
       <p>Daily Challenge: {dc} x 2 = {dc * 2}</p>
-      <p>Dailt Test: {dt} x 20 = {dt * 20}</p>
+      <p>Daily Test: {dt} x 20 = {dt * 20}</p>
       <hr />
       <p><strong>Total Points: {totalPoints}</strong></p>
     </aside>
