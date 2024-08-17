7b7c976ac09ccc2a37d52fa3b1813572214267ef
Guru <guru@duck.com>
2024-08-17 14:59:44 +0530

    added source code for android

---
+++ android/app/src/main/java/in/gururaja/skillrack/DelegationService.java
@@ -0,0 +1,14 @@
+package in.gururaja.skillrack;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
