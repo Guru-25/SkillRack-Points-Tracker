2fa0f643a58decd7d0b8cd295b17b979f67e3236
Guru <guru@duck.com>
2024-08-23 19:03:25 +0530

    remove status feature

--- client/src/App.js
+++ client/src/App.js
@@ -300,17 +300,6 @@ const App = () => {
             </a>
           )}
           <br /><br /> */}
-          {!state.isValidUrl && (
-            <>
-              <a href="https://status.gururaja.in" target="_blank" rel="noopener noreferrer">
-                <img
-                  src="https://uptime.betterstack.com/status-badges/v1/monitor/1ii67.svg"
-                  alt="SkillRack Status"
-                />
-              </a>
-              <br /><br /><br />
-            </>
-          )}
           {state.isValidUrl && (
             <>
               <aside>
